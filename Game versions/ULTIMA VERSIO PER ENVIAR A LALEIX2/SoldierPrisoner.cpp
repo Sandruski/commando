7ba62f068877f07a37efcbb89395e457eec6450b
DatBeQuiet <kayter72@gmail.com>
2017-04-26 14:57:48 +0200

    For Aleix2

---
+++ Game versions/ULTIMA VERSIO PER ENVIAR A LALEIX2/SoldierPrisoner.cpp
@@ -0,0 +1,63 @@
+#include "Application.h"
+#include "SoldierPrisoner.h"
+#include "ModuleCollision.h"
+#include "ModulePlayer.h"
+#include "ModuleUI.h"
+#include "ModuleEnemies.h"
+#include "SDL/include/SDL_timer.h"
+
+Enemy_SoldierPrisoner::Enemy_SoldierPrisoner(int x, int y) : Enemy(x, y)
+{
+	//ANIMATION MOVE (normal SOLDIER)
+	//following spritesheet order (left-to-right)
+	movepoints.PushBack({ 6, 195, 16, 22 });
+	movepoints.PushBack({ 24, 195, 16, 22 });
+	movepoints.PushBack({ 42, 195, 15, 22 });
+
+	movepoints.speed = 0.1f;
+
+	die.PushBack({ 6, 111, 16, 27 });
+	die.PushBack({ 40, 104, 15, 27 });
+	die.PushBack({ 25, 111, 16, 27 });
+	die.PushBack({ 40, 104, 15, 27 });
+
+	animation = &movepoints;
+
+	collider = App->collision->AddCollider({ 0, 0, 18, 18 }, COLLIDER_TYPE::COLLIDER_ENEMY, (Module*)App->enemies);
+}
+
+void Enemy_SoldierPrisoner::Move()
+{
+	currentTime = SDL_GetTicks();
+	currentTime -= lastTime;
+
+	if (dieB == false) {
+		lastTime = SDL_GetTicks();
+		if ((position.y >= 1481 - 2656) && (App->player->position.y <= 1712 - 2656))
+			position.y--;
+		else if (position.y == 1480 - 2656) {
+			animation = nullptr;
+			collider->to_delete = true;
+		}
+
+	}
+	else {
+		animation = &die;
+		die.speed = 0.1f;
+		if (currentTime > 800)
+			Esperanza = false;
+	}
+}
+
+void Enemy_SoldierPrisoner::OnCollision(Collider* c1, Collider* c2) {
+
+	if (dieB == false) {
+		if (c2->type == COLLIDER_PLAYER_SHOT)
+			App->UI->score += 75;
+		else if (c2->type == COLLIDER_END_OF_GRENADE)
+			App->UI->score += 150;
+	}
+	dieB = true;
+	App->enemies->dieE++;
+}
+
